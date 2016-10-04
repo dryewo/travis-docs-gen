ea702d418fe85ec64ce3e669fc27ecd07e7ca969
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-10-04 23:13:48 +0200

    Fix deployment condition, replace compromised token.

--- gendoc.sh
+++ gendoc.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 IFS=$'\t\n'
 
-ls -la
-env
-
 git clone -b gh-pages "https://github.com/${TRAVIS_REPO_SLUG}.git" gh-pages
 
 DOCS_DIR=gh-pages/$TRAVIS_BRANCH
@@ -14,12 +11,13 @@ mkdir -p "$DOCS_DIR"
 # "Build" the documentation
 cp index.tmpl "$DOCS_DIR/index.html"
 
-if [[ -n $TRAVIS_PULL_REQUEST ]]; then
+echo "Documentation successfully built"
+
+if [[ -n $TRAVIS_PULL_REQUEST_BRANCH ]]; then
     # We are building a pull request, nothing to do here
     echo "Building a PR with ID=$TRAVIS_PULL_REQUEST, skipping the deployment."
     exit 0
 fi
-
 
 cd "$DOCS_DIR"
 
@@ -30,3 +28,5 @@ git config user.email "travis@travis-ci.org"
 git add --all
 git commit -m "Deploy code docs to GitHub Pages Travis build: $TRAVIS_BUILD_NUMBER" -m "Commit: $TRAVIS_COMMIT"
 git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
+
+echo "Documentation successfully published"
